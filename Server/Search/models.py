d0599a1860f64ae03e9ab5d78df922ca06bbbb4c
Blaxon <asasdyy@163.com>
2015-11-13 11:51:46 +0800

    Add GameInfo model into Search app

--- Server/Search/models.py
+++ Server/Search/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class GameInfo(models.Model):
+    name = models.TextField(primary_key=True)
+    simple_intro = models.TextField()
+    type = models.TextField()
+    language = models.TextField()
+    display = models.TextField()
+    theme = models.TextField()
+    company = models.TextField()
+    time = models.TextField()
+    tag = models.TextField()
+    player_vote = models.TextField()
+    score = models.FloatField()
+    introduction = models.TextField()
+    pircture = models.TextField()
+
