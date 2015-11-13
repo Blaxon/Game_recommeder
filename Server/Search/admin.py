0c818ba1f5d89ad6e1b540a998de248b88528881
Blaxon <asasdyy@163.com>
2015-11-13 16:35:48 +0800

    Customize the admin view

--- Server/Search/admin.py
+++ Server/Search/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import GameInfo
+
+
+class GameInfoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'theme', 'player_vote', 'time', 'score')
+
+
+admin.site.register(GameInfo, GameInfoAdmin)
