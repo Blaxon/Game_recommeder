c1506ce3e1d6c09300d8c8b02258eb4d1b845528
Blaxon <asasdyy@163.com>
2015-11-23 16:18:59 +0800

    Complete the content of GameInfoIndex

--- Server/Search/search_indexes.py
+++ Server/Search/search_indexes.py
@@ -3,7 +3,20 @@ from haystack import indexes
 
 
 class GameInfoIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True,model_attr='name')
+    text = indexes.CharField(document=True, use_template=True, model_attr='name')
+    name = indexes.CharField(model_attr='name')
+    simple_intro = indexes.CharField(model_attr='simple_intro')
+    type = indexes.CharField(model_attr='type')
+    language = indexes.CharField(model_attr='language')
+    display = indexes.CharField(model_attr='display')
+    theme = indexes.CharField(model_attr='theme')
+    company = indexes.CharField(model_attr='company')
+    time = indexes.CharField(model_attr='time')
+    tag = indexes.CharField(model_attr='tag')
+    player_vote = indexes.CharField(model_attr='player_vote')
+    game_score = indexes.FloatField(model_attr='score')  # attention score in model change into game_score
+    introduction = indexes.CharField(model_attr='introduction')
+    picture = indexes.CharField(model_attr='picture')
 
     def get_model(self):
         return GameInfo
