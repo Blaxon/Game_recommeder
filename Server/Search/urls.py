16592571e6385909820acc0856100f57ce137381
Blaxon <asasdyy@163.com>
2015-11-23 16:18:01 +0800

    A simple form example

--- Server/Search/urls.py
+++ Server/Search/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'test/', include('haystack.urls')),
+    url(r'example/', 'Search.views.example_input', name='example'),  # 简单的信息接受示例
+    url(r'form/', views.form, name='form'),
 ]
--- Server/Search/views.py
+++ Server/Search/views.py
@@ -6,3 +6,13 @@ from django.http import HttpResponse
 
 def index(req):
     return HttpResponse("Hello World.You're at the Search index.")
+
+
+def form(req):  # 表页面函数
+    return render(req, 'form.html')
+
+
+def example_input(req):  # 处理表函数的应用
+    a = req.GET['a']
+    b = req.GET['b']
+    return HttpResponse(int(a)+int(b))
