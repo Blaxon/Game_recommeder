e0b908b14defecacf59c6e78fee6bb5f73d0e287
Blaxon <asasdyy@163.com>
2015-11-24 16:36:37 +0800

    Add user model into Database

--- Server/User/models.py
+++ Server/User/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(primary_key=True, max_length=100)
+    games = models.TextField(null=True)
+    times = models.TextField(null=True)
+
+    def __str__(self):
+        return self.username
